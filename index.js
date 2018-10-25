b8c84001a514c3a7a58cd60176df85954f0f6ac1
Gregor <gregor@martynus.net>
2018-10-24 23:28:27 -0700

    wip

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = GitHubApi
 
-const defaultsDeep = require('lodash/defaultsDeep')
 const Hook = require('before-after-hook')
+const isPlainObject = require('is-plain-object')
+const merge = require('deepmerge')
 
 const deprecate = require('./lib/deprecate')
 const parseClientOptions = require('./lib/parse-client-options')
@@ -14,34 +15,36 @@ const PLUGINS = [
 ]
 
 function GitHubApi (options) {
-  const defaults = defaultsDeep(parseClientOptions(options), request.endpoint.DEFAULTS)
-
+  const { DEFAULTS } = request.endpoint.defaults(parseClientOptions(options))
   const hook = new Hook()
   const api = {
     // NOTE: github.hook and github.plugin are experimental APIs
     //       at this point and can change at any time
     hook,
     plugin: (pluginFunction) => pluginFunction(api),
-    request: (options) => api.hook('request', defaultsDeep(options, defaults), (options) => {
+    request: (options) => {
       if (options.input) {
         options.data = options.input
         delete options.input
         deprecate('options.input – is now options.data')
       }
 
-      return request(options)
+      options = merge(DEFAULTS, options, { isMergeableObject: isPlainObject })
+      return api.hook('request', options, (options) => {
+        return request(options)
 
-        // deprecate response.meta
-        .then(response => {
-          Object.defineProperty(response, 'meta', {
-            get () {
-              deprecate('response.meta – use response.headers instead (#896)')
-              return response.headers
-            }
+          // deprecate response.meta
+          .then(response => {
+            Object.defineProperty(response, 'meta', {
+              get () {
+                deprecate('response.meta – use response.headers instead (#896)')
+                return response.headers
+              }
+            })
+            return response
           })
-          return response
-        })
-    })
+      })
+    }
   }
 
   PLUGINS.forEach(api.plugin)
