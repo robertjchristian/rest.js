9e4e04faa3bd07c05700dcf2a23253bf1738154f
Gregor <gregor@martynus.net>
2018-10-24 17:30:14 -0700

    refactor: use @octokit/request
    BREAKING CHANGE: error.headers has been removed. error.toJSON() has been removed. error.toString() is no longer overwritten, the returned string now starts with `HttpError: `.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ module.exports = GitHubApi
 const defaultsDeep = require('lodash/defaultsDeep')
 const Hook = require('before-after-hook')
 
+const deprecate = require('./lib/deprecate')
 const parseClientOptions = require('./lib/parse-client-options')
-const request = require('./lib/request')
-const ENDPOINT_DEFAULTS = require('./lib/endpoint').DEFAULTS
+const request = require('@octokit/request')
 
 const PLUGINS = [
   require('./lib/plugins/authentication'),
@@ -14,15 +14,34 @@ const PLUGINS = [
 ]
 
 function GitHubApi (options) {
-  const defaults = defaultsDeep(parseClientOptions(options), ENDPOINT_DEFAULTS)
+  const defaults = defaultsDeep(parseClientOptions(options), request.endpoint.DEFAULTS)
 
   const hook = new Hook()
   const api = {
-    // NOTE: github.hook, github.plugin and github.request are experimental APIs
+    // NOTE: github.hook and github.plugin are experimental APIs
     //       at this point and can change at any time
     hook,
     plugin: (pluginFunction) => pluginFunction(api),
-    request: (options) => api.hook('request', defaultsDeep(options, defaults), request)
+    request: (options) => api.hook('request', defaultsDeep(options, defaults), (options) => {
+      if (options.input) {
+        options.data = options.input
+        delete options.input
+        deprecate('options.input – is now options.data')
+      }
+
+      return request(options)
+
+        // deprecate response.meta
+        .then(response => {
+          Object.defineProperty(response, 'meta', {
+            get () {
+              deprecate('response.meta – use response.headers instead (#896)')
+              return response.headers
+            }
+          })
+          return response
+        })
+    })
   }
 
   PLUGINS.forEach(api.plugin)
