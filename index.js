80757f757223edaa72a00318a724c357f3795d19
Gregor <gregor@martynus.net>
2018-10-26 15:30:41 -0700

    wip

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 module.exports = GitHubApi
 
 const Hook = require('before-after-hook')
-const isPlainObject = require('is-plain-object')
-const merge = require('deepmerge')
 
-const deprecate = require('./lib/deprecate')
 const parseClientOptions = require('./lib/parse-client-options')
 const request = require('@octokit/request')
 
@@ -15,36 +12,13 @@ const PLUGINS = [
 ]
 
 function GitHubApi (options) {
-  const { DEFAULTS } = request.endpoint.defaults(parseClientOptions(options))
   const hook = new Hook()
   const api = {
     // NOTE: github.hook and github.plugin are experimental APIs
     //       at this point and can change at any time
     hook,
     plugin: (pluginFunction) => pluginFunction(api),
-    request: (options) => {
-      if (options.input) {
-        options.data = options.input
-        delete options.input
-        deprecate('options.input – is now options.data')
-      }
-
-      options = merge(DEFAULTS, options, { isMergeableObject: isPlainObject })
-      return api.hook('request', options, (options) => {
-        return request(options)
-
-          // deprecate response.meta
-          .then(response => {
-            Object.defineProperty(response, 'meta', {
-              get () {
-                deprecate('response.meta – use response.headers instead (#896)')
-                return response.headers
-              }
-            })
-            return response
-          })
-      })
-    }
+    request: request.defaults(parseClientOptions(options))
   }
 
   PLUGINS.forEach(api.plugin)
